c32d2bc650724947e0bd52c0a84f8f00a5aee1de
jlin03 <jlin03@stuy.edu>
2018-09-18 16:34:28 -0400

    fixed code

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,14 +1,14 @@
-public class CtoFTester.java() {
-	public double celsiusToFahrenheit(double n) {
-		return ((n - 32) * (5 / 9));
+public class CtoFTester {
+	public static double celsiusToFahrenheit(double n) {
+		return ((n - 32.0) * (5.0 / 9.0));
 	}
 	
-	public double fahrenheittoCelsius(double n) {
-		return ((n * (9 / 5)) + 32);
+	public static double fahrenheitToCelsius(double n) {
+		return ((n * (9.0 / 5.0)) + 32.0);
 	}
 	
 	public static void main(String[] args) {
-		celsiusToFahrenheit(64);
-		fahrenheittoCelsius(50);
+		System.out.println(celsiusToFahrenheit(64));
+		System.out.println(fahrenheitToCelsius(50));
 	}
 }
